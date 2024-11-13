7a5b3410e00a483b8df308e317df9cfcbd4850da
nareshgantala <nareshgantala@gmail.com>
2024-11-13 11:24:03 +0530

    oper

---
+++ part1/operations.js
@@ -0,0 +1,11 @@
+let score = 102;
+let bonus = 25;
+
+let totalScore = score + bonus
+
+let addition = 4 + 9
+let subtraction = 9 - 3
+let mul = 3 * 5
+let div = 8 / 2
+let reminder = 9 % 2
+let expo = 2 ** 3
