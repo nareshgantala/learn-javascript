9a16a21bc8e1eb85f6ba9b05b615077ed3485a5e
nareshgantala <nareshgantala@gmail.com>
2024-11-13 11:32:38 +0530

    oper

--- part1/operations.js
+++ part1/operations.js
@@ -9,3 +9,21 @@ let mul = 3 * 5
 let div = 8 / 2
 let reminder = 9 % 2
 let expo = 2 ** 3
+
+let myscore = 110
+myscore++
+console.log(myscore);
+
+let credits = 56
+credits--
+console.log(credits);
+
+let num1 = 6
+let num2 = 3
+let num3 = 3
+
+console.log(num1 == num2);
+console.log(num2 == num3);
+
+
+
