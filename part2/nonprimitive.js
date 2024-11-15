1984616ef60ef18bf5527b8f15d02d813b55d46a
nareshgantala <nareshgantala@gmail.com>
2024-11-15 18:30:23 +0530

    call

--- part2/nonprimitive.js
+++ part2/nonprimitive.js
@@ -3,11 +3,12 @@ let username = {
   lastname: "Gantala",
 };
 
+username.isLoggedIn = true
 console.log(username.firstname);
 console.log(username["lastname"]);
 console.log(username);
 console.log(typeof(username));
-
+console.log(username.isLoggedIn);
 
 
 
