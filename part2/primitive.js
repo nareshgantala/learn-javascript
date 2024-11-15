b344f45b840c306b358039acf178739a5aa8f622
nareshgantala <nareshgantala@gmail.com>
2024-11-15 13:27:23 +0530

    primitive

---
+++ part2/primitive.js
@@ -0,0 +1,10 @@
+// number
+let balance = 200;
+
+let anotherBalance = new Number(120);
+
+console.log(balance);
+console.log(anotherBalance.valueOf());
+
+console.log(typeof balance);
+console.log(typeof anotherBalance);
