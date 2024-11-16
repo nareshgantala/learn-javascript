19b147796a1041741dc32b4a53d2e7d623b3f17c
nareshgantala <nareshgantala@gmail.com>
2024-11-16 15:33:42 +0530

    challenge

---
+++ part3/challenge.js
@@ -0,0 +1,5 @@
+let teaFlavours = ["green tea","black tea","oolang tea"];
+const firstTea = teaFlavours[0];
+
+let cities = ["London", "Tokyo", "Paris", "New York"];
+const favoriteCity = cities[2];
