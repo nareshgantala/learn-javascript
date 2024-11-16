14f76df50cb2802d39a411dbb73be2c3777dd4b5
nareshgantala <nareshgantala@gmail.com>
2024-11-16 15:42:02 +0530

    log

--- part3/challenge.js
+++ part3/challenge.js
@@ -1,5 +1,14 @@
-let teaFlavours = ["green tea","black tea","oolang tea"];
+let teaFlavours = ["green tea", "black tea", "oolang tea"];
 const firstTea = teaFlavours[0];
+console.log(firstTea);
+
 
 let cities = ["London", "Tokyo", "Paris", "New York"];
 const favoriteCity = cities[2];
+console.log(favoriteCity);
+
+
+let teaTypes = ["herbal tea", "white tea", "masala chai"];
+teaTypes[1] = "jasmine tea";
+console.log(teaTypes);
+
