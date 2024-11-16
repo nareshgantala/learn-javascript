c703874cdf59c242d9120494c1a68a0e5d3ddbd4
nareshgantala <nareshgantala@gmail.com>
2024-11-16 08:29:45 +0530

    x

--- part3/conditions.js
+++ part3/conditions.js
@@ -7,21 +7,19 @@ console.log("Uppr Code");
 
 if (num1 > num2) {
   console.log("num1 is greater than num2");
-}else {
-    console.log("Nope, num1 is not greater");
-    
+} else {
+  console.log("Nope, num1 is not greater");
 }
 
 console.log("lower code");
 
 // check if strings are equal
 
-let username = "Naresh"
-let anotherUsername = "Naresh"
+let username = "Naresh";
+let anotherUsername = "Naresh";
 
-if (username == anotherUsername) {
-    console.log("Pick another username");
-}else {
-    console.log("use this user");
-    
+if (username != anotherUsername) {
+  console.log("Pick another username");
+} else {
+  console.log("use this user");
 }
