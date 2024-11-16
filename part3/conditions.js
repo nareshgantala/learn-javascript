d3b6cfc9e56ff9d7aac89da8b289fe9a6cf42f85
nareshgantala <nareshgantala@gmail.com>
2024-11-16 08:18:12 +0530

    conditions

---
+++ part3/conditions.js
@@ -0,0 +1,12 @@
+// check if num1 is greater than num2
+
+let num1 = 5;
+let num2 = 10;
+
+console.log("Uppr Code");
+
+if (num1 > num2) {
+  console.log("num1 is greater than num2");
+}
+
+console.log("lower code");
