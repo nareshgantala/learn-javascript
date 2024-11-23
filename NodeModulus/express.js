f6dee8f1510360ad1e2df620d07eac9ae7e40ee2
nareshgantala <nareshgantala@gmail.com>
2024-11-23 15:15:13 +0530

    route

--- NodeModulus/express.js
+++ NodeModulus/express.js
@@ -6,6 +6,10 @@ app.get("/apple", (req, res) => {
   res.send("Apple is in red colour");
 });
 
+app.get("/orange", (req, res) => {
+  res.send("Orange is in yellow colour");
+});
+
 app.listen(port, () => {
   console.log("server running suceessfully on port 5000");
 });
