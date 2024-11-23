6cd8efd014c4e349f4802491937ec59bf30ac70a
nareshgantala <nareshgantala@gmail.com>
2024-11-23 20:11:13 +0530

    mong

---
+++ NodeModulus/mongoose.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://naresh:abc1234@cluster1.uthww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
+  )
+  .then(() => {
+    console.log("Mongo DB connected successfully");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
