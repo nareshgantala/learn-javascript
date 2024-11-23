11834364ad13221a4293e9a7a0b1d75f2cb0f99b
nareshgantala <nareshgantala@gmail.com>
2024-11-23 11:38:00 +0530

    data eneter

--- NodeModulus/fs.js
+++ NodeModulus/fs.js
@@ -12,3 +12,13 @@ fs.writeFile("demo.html", "utf8", (err) => {
   }
   console.log("New file created");
 });
+
+const content = "My Nane is Naresh";
+
+fs.writeFile("demo.html", content, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("data enetered successfully");
+  }
+});
