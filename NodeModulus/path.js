3b34c26e7ec06c64b98884e20499850372f9c4c1
nareshgantala <nareshgantala@gmail.com>
2024-11-23 09:06:22 +0530

    filename

--- NodeModulus/path.js
+++ NodeModulus/path.js
@@ -1,2 +1,6 @@
-console.log(__filename);
-console.log(__dirname);
+// console.log(__filename);
+// console.log(__dirname);
+
+const path = require("path");
+file_name = path.filename(__filename);
+console.log(file_name);
