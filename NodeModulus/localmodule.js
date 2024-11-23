1a08cacecbc48ae8c2b2feb835ec23d42b0bdb6e
nareshgantala <nareshgantala@gmail.com>
2024-11-23 14:48:14 +0530

    up

--- NodeModulus/localmodule.js
+++ NodeModulus/localmodule.js
@@ -1,4 +1,4 @@
-const { add, sub, mul, div } = require("./demomodule");
+const { add, sub, mul, div } = require("./demomodules");
 console.log(add(10, 10));
 console.log(sub(20, 10));
 console.log(mul(30, 10));
