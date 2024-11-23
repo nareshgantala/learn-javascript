f0eeb76585a6d9c7afad987299b85a1f83f22426
nareshgantala <nareshgantala@gmail.com>
2024-11-23 13:00:15 +0530

    up

--- NodeModulus/http.js
+++ NodeModulus/http.js
@@ -1,6 +1,6 @@
 const server = require("http");
-server.createServer((req, res) => {
+const myServer = server.createServer((req, res) => {
   res.write("Hello India!");
   res.end();
 });
-server.listen(5000);
+myServer.listen(5000);
