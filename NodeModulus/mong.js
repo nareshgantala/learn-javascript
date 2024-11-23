9841c1dcc1b9ab4cf70787dc77f9a20e65d98816
nareshgantala <nareshgantala@gmail.com>
2024-11-23 19:28:00 +0530

    connect mongo

--- NodeModulus/mong.js
+++ NodeModulus/mong.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const dotEnv = require("dotenv");
-const { log } = require("console");
 const app = express();
+const { MongoClient } = require("mongodb");
 const PORT = 5000;
+
+dotEnv.config();
+MongoClient.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Mongo DB connected successfully");
+  })
+  .catch(error);
 
 app.listen(PORT, () => {
   console.log("server started");
