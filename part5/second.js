7698b5e18a4fca4f60c38ee8edcbe6909e05c023
Naresh Gantala <nareshgantala@gmail.com>
2026-01-21 13:28:02 +0530

    type conversions

---
+++ part5/second.js
@@ -0,0 +1,17 @@
+let x = String(-2)
+console.log(x, typeof x);
+
+x = 8
+console.log(x, typeof x);
+
+x = x + "Naresh"
+console.log(x, typeof x);
+
+x = x - 2
+console.log(x, typeof x);
+
+
+console.log(Boolean(1));
+
+
+
