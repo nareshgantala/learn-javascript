d5b111879efcc4a18fcd6a8e0c77d19906a49196
Naresh Gantala <nareshgantala@gmail.com>
2026-01-23 20:43:59 +0530

    pre and post increments

---
+++ part5/fifth.js
@@ -0,0 +1,10 @@
+let num1 = 5
+
+let x = num1++   //post increment
+
+console.log(x, num1);
+
+let y = ++num1    //pre increment 
+
+console.log(y, num1);
+
---
+++ part5/fourth.js
@@ -0,0 +1,10 @@
+let num1 = true
+let num2 = true
+
+let result = num1 + num2
+
+console.log(result);
+
+let result2 = num1 -num2
+console.log(result2);
+
